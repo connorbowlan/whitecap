6559078652177cae62c9a4347278e0f660267129
connorbowlan <connor@cbowlan.com>
2023-05-29 18:46:52 -0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+import wheatFields from "/img/hero/wheat-fields.jpg";
+
+function Hero() {
+  return <div className="hero-1"></div>;
+}
+
+export default Hero;
---
+++ src/components/index.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import Business from "./Business";
+import Clients from "./Clients";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Logo from "./Logo";
+
+export {
+  Logo,
+  Navbar,
+  Business,
+  Clients,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
