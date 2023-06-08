f632558e19288babf09998eecf56b7d388c2632e
Owen <socknot@live.com>
2023-06-08 00:45:01 -0700

    color changes

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -7,69 +7,59 @@ function Home() {
         <h1 className="text-4xl font-bold mb-6 text-white bg-gradient-to-r from-blue-500 to-green-700 p-4 rounded-lg shadow-xl">
           Welcome to Whitecap Energy
         </h1>
-        <p className="text-lg text-gray-700 bg-white bg-opacity-80 p-4 rounded-lg shadow-xl">
-          We are a leading provider of natural gas solutions, committed to sustainability and customer satisfaction.
+        <p className="text-lg text-dark-1000 bg-white bg-opacity-80 p-4 rounded-lg shadow-xl">
+          We are a leading provider of natural gas solutions, committed to <em style={{ fontStyle: 'italic', transform: 'skew(-10deg)' }}>sustainability and customer satisfaction</em>.
         </p>
       </section>
 
       <section className="mb-12">
         <h2 className="text-2xl font-bold mb-6">Why Choose Natural Gas?</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="bg-gradient-to-r from-green-500 to-green-700 text-white p-4 rounded-lg shadow-xl">
+          <div className="bg-gradient-to-r from-blue-500 to-green-700 text-white p-4 rounded-lg shadow-xl">
             <h3 className="text-xl font-medium mb-2">Cleaner Energy</h3>
             <p className="text-gray-300">
               Natural gas is a cleaner energy source with lower carbon emissions. It is widely used for electricity generation and is environmentally friendly.
             </p>
           </div>
-          <div className="bg-gradient-to-r from-green-500 to-green-700 text-white p-4 rounded-lg shadow-xl">
+          <div className="bg-gradient-to-r from-green-700 to-blue-500 text-white p-4 rounded-lg shadow-xl">
             <h3 className="text-xl font-medium mb-2">Saving Money</h3>
             <p className="text-gray-300">
               Natural gas is cost-effective, saving money on energy bills. It is abundant, domestically produced, and has long-lasting appliances.
             </p>
           </div>
-          <div className="bg-gradient-to-r from-green-500 to-green-700 text-white p-4 rounded-lg shadow-xl">
-            <h3 className="text-xl font-medium mb-2">Better Environmentally</h3>
-            <p className="text-gray-300">
-              Natural gas produces fewer harmful emissions than other fossil fuels. It is a better option for the environment and can be used with renewable resources.
-            </p>
-          </div>
+          
         </div>
       </section>
 
       <section className="mb-12">
         <h2 className="text-2xl font-bold mb-6">Why Choose Whitecap Energy?</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="bg-gradient-to-r from-orange-500 to-red-700 text-white p-4 rounded-lg shadow-xl">
+          <div className="bg-gradient-to-r from-green-500 to-red-700 text-white p-4 rounded-lg shadow-xl">
             <h3 className="text-xl font-medium mb-2">Reliable Service</h3>
             <p className="text-gray-300">
               At Whitecap Energy, we prioritize reliability and ensure that our customers receive consistent and uninterrupted natural gas supply.
             </p>
           </div>
-          <div className="bg-gradient-to-r from-orange-500 to-red-700 text-white p-4 rounded-lg shadow-xl">
+          <div className="bg-gradient-to-r from-red-700 to-green-500 text-white p-4 rounded-lg shadow-xl">
             <h3 className="text-xl font-medium mb-2">Exceptional Customer Support</h3>
             <p className="text-gray-300">
               Our dedicated customer support team is available 24/7 to assist you with any inquiries or concerns regarding your natural gas service.
             </p>
           </div>
-          <div className="bg-gradient-to-r from-orange-500 to-red-700 text-white p-4 rounded-lg shadow-xl">
-            <h3 className="text-xl font-medium mb-2">Commitment to Sustainability</h3>
-            <p className="text-gray-300">
-              We are committed to sustainable practices and actively contribute to reducing carbon footprint through our natural gas solutions.
-            </p>
-          </div>
+         
         </div>
       </section>
 
       <section className="mb-12">
         <h2 className="text-2xl font-bold mb-6">Customer Testimonials</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="bg-gradient-to-r from-green-500 to-red-700 text-white p-4 rounded-lg shadow-xl">
+          <div className="bg-gradient-to-r from-yellow-700 to-red-600 text-white p-4 rounded-lg shadow-xl">
             <blockquote className="italic text-gray-300">
               "Whitecap Energy has been an excellent natural gas provider. Their service is reliable and their prices are competitive. Highly recommended!"
               <cite className="block mt-4 text-gray-400 font-medium">- John Smith</cite>
             </blockquote>
           </div>
-          <div className="bg-gradient-to-r from-red-500 to-green-700 text-white p-4 rounded-lg shadow-xl">
+          <div className="bg-gradient-to-r from-red-700 to-yellow-600 text-white p-4 rounded-lg shadow-xl">
             <blockquote className="italic text-gray-300">
               "Switching to natural gas with Whitecap Energy has been a game-changer. Not only am I saving money, but I also feel good about reducing my carbon footprint."
               <cite className="block mt-4 text-gray-400 font-medium">- Emily Johnson</cite>
