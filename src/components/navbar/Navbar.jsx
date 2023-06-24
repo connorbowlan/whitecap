7d9ccb2537aedac56bf3e173c290eb6bc243f3da
connorbowlan <connor@cbowlan.com>
2023-06-24 12:51:55 -0700

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,73 +9,76 @@ function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <nav>
-      <div className="navbar">
-          <div className="logo">
-            <img src={logo} alt="WhiteCap Logo" />
-          </div>
-          <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#whitecap">Information</a>
-            </li>
-            <li>
-              <a href="#possibility">Natural Gas</a>
-            </li>
-            <li>
-              <a href="#features">Case Studies</a>
-            </li>
-            <li>
-              <a href="#blog">News</a>
-            </li>
-          </ul>
-        {/* <div className="navbar-sign">
+    <nav className="gradient-bg">
+      <div className="logo">
+        <img src={logo} alt="WhiteCap Logo" />
+      </div>
+      <div class="horizontal-links">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#whitecap">Information</a>
+          </li>
+          <li>
+            <a href="#possibility">Natural Gas</a>
+          </li>
+          <li>
+            <a href="#features">Case Studies</a>
+          </li>
+          <li>
+            <a href="#blog">News</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* <div className="navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div> */}
-        <div className="navbar-menu">
-          {toggleMenu ? (
-            <RiCloseLine
-              color="#ffffff"
-              size={27}
-              onClick={() => setToggleMenu(false)}
-            />
-          ) : (
-            <RiMenu3Line
-              color="#ffffff"
-              size={27}
-              onClick={() => setToggleMenu(true)}
-            />
-          )}
-          {toggleMenu && (
-            <div className="navbar-menu_container">
-              <div className="navbar-menu_container-links">
-                <p>
+      <div className="navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#ffffff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#ffffff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="navbar-menu_container">
+            <div className="navbar-menu_container-links">
+              <ul>
+                <li>
                   <a href="#home">Home</a>
-                </p>
-                <p>
-                  <a href="#about">About Us</a>
-                </p>
-                <p>
-                  <a href="#services">Our Services</a>
-                </p>
-                <p>
-                  <a href="#products">Products</a>
-                </p>
-                <p>
-                  <a href="#contact">Contact Us</a>
-                </p>
-              </div>
+                </li>
+                <li>
+                  <a href="#whitecap">Information</a>
+                </li>
+                <li>
+                  <a href="#possibility">Natural Gas</a>
+                </li>
+                <li>
+                  <a href="#features">Case Studies</a>
+                </li>
+                <li>
+                  <a href="#blog">News</a>
+                </li>
+              </ul>
+            </div>
 
-              <div className="navbar-menu_container-links-sign">
+            {/* <div className="navbar-menu_container-links-sign">
                 <p>Sign in</p>
                 <button type="button">Sign up</button>
-              </div>
-            </div>
-          )}
-        </div>
+              </div> */}
+          </div>
+        )}
       </div>
     </nav>
   );
