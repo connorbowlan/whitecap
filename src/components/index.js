bb541d9e6767b207fd7867039b99532fe8559ef0
Connor Bowlan <connor.bowlan@project.com>
2025-10-02 14:28:42 -0700

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as NewsFeature } from "./newsFeature/NewsFeature";
-export { default as CTA } from "./cta/CTA";
-export { default as InfoFeature } from "./infoFeature/InfoFeature";
-export { default as Navbar } from "./navigation/Navbar";
-export { default as Faq } from "./faq/Faq";
